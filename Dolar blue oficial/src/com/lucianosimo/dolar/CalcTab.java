13b22afb1ea9be202657e33a2a0489236b1ecf63
lucianosimo <lucianosimo87@gmail.com>
2014-06-25 22:06:56 -0300

    Calculador pesos a dolares avanzando

--- Dolar blue oficial/src/com/lucianosimo/dolar/CalcTab.java
+++ Dolar blue oficial/src/com/lucianosimo/dolar/CalcTab.java
@@ -1,5 +1,7 @@
 package com.lucianosimo.dolar;
 
+import java.text.DecimalFormat;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -7,15 +9,23 @@ import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class CalcTab extends Fragment{
 
 	private TextView valueConversorOficial;
+	private TextView labelConversorOficial;
 	private EditText editValor;
+	
+	private float valorOficial;
+	private float newOficialSell;
+	
 	private Context context = null;
 	
 	@Override
@@ -35,11 +45,25 @@ public class CalcTab extends Fragment{
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-		float newOficialSell = sharedPreferences.getFloat("newOficialSell", 0);
-		/*valueConversorOficial = (TextView) getView().findViewById(R.id.valueConversorOficial);
-		editValor = (EditText) getView().findViewById(R.id.editValor);
-		float valorOficial = Float.parseFloat(editValor.getText().toString()) * newOficialSell;
-		valueConversorOficial.setText("" + valorOficial);
-		super.onViewCreated(view, savedInstanceState);*/
+		final DecimalFormat format = new DecimalFormat("0.00");
+		newOficialSell = sharedPreferences.getFloat("newOficialSell", 0);
+		
+		valueConversorOficial = (TextView) getView().findViewById(R.id.valueConversorOficial);
+		labelConversorOficial = (TextView) getView().findViewById(R.id.labelConversorOficial);
+		labelConversorOficial.setText("Pesos");
+		editValor = (EditText) getView().findViewById(R.id.editValor);		
+		
+		ImageView calcularButton = (ImageView) getView().findViewById(R.id.calcular_imagen);
+		calcularButton.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(editValor.getWindowToken(), 0);
+				valorOficial = Float.parseFloat(editValor.getText().toString()) * newOficialSell;
+				valueConversorOficial.setText("$ " + format.format(valorOficial));				
+			}
+		});
+		
+		super.onViewCreated(view, savedInstanceState);
 	}
 }
