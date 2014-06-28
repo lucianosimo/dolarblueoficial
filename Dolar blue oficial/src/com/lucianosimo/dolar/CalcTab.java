a437521a6cfa6820f998fca06d91d9336ebca63b
lucianosimo <lucianosimo87@gmail.com>
2014-06-28 20:55:25 -0300

    Finalizado calculador

--- Dolar blue oficial/src/com/lucianosimo/dolar/CalcTab.java
+++ Dolar blue oficial/src/com/lucianosimo/dolar/CalcTab.java
@@ -13,6 +13,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -21,14 +22,21 @@ public class CalcTab extends Fragment{
 
 	private TextView valueConversorOficial;
 	private TextView labelConversorOficial;
-	private TextView labelDolaresPesos;
-	private TextView labelPesosDolares;
-	private ImageView tab1;
-	private ImageView tab2;
+	private TextView labelConversorBlue;
+	private TextView valueConversorBlue;
+	private FrameLayout lineConversorBlue;
+	private TextView labelConversorCard;
+	private TextView valueConversorCard;
+	private FrameLayout lineConversorCard;
+	
+	private ImageView button_dap;
+	private ImageView button_pad;
 	private EditText editValor;
 	
 	private float valorOficial;
+	private float valorBlue;
 	private float newOficialSell;
+	private float newBlueSell;
 	
 	private Context context = null;
 	
@@ -51,24 +59,55 @@ public class CalcTab extends Fragment{
 		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
 		final DecimalFormat format = new DecimalFormat("0.00");
 		newOficialSell = sharedPreferences.getFloat("newOficialSell", 0);
+		newBlueSell = sharedPreferences.getFloat("newBlueSell", 0);
 		
+		editValor = (EditText) getView().findViewById(R.id.editValor);
+		editValor.setText("");
 		valueConversorOficial = (TextView) getView().findViewById(R.id.valueConversorOficial);
 		labelConversorOficial = (TextView) getView().findViewById(R.id.labelConversorOficial);
-		labelDolaresPesos = (TextView) getView().findViewById(R.id.labelDolaresPesos);
-		labelPesosDolares = (TextView) getView().findViewById(R.id.labelPesosDolares);
-		tab1 = (ImageView) getView().findViewById(R.id.tab1);
-		tab2 = (ImageView) getView().findViewById(R.id.tab2);
-		final ImageView calcularButton = (ImageView) getView().findViewById(R.id.calcular_imagen);
+		button_dap = (ImageView) getView().findViewById(R.id.tab1);
+		button_pad = (ImageView) getView().findViewById(R.id.tab2);
+		labelConversorBlue = (TextView) getView().findViewById(R.id.labelConversorBlue);
+		valueConversorBlue = (TextView) getView().findViewById(R.id.valueConversorBlue);
+		lineConversorBlue = (FrameLayout) getView().findViewById(R.id.lineValue2);
+		labelConversorCard = (TextView) getView().findViewById(R.id.labelConversorCard);
+		valueConversorCard = (TextView) getView().findViewById(R.id.valueConversorCard);
+		lineConversorCard = (FrameLayout) getView().findViewById(R.id.lineValue3);
 		
-		labelDolaresPesos.setOnClickListener(new OnClickListener() {
+		final ImageView calcularButton = (ImageView) getView().findViewById(R.id.calcular_imagen);
+		calcularButton.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(editValor.getWindowToken(), 0);
+				String stringValue = editValor.getText().toString();
+				if (stringValue.equals("")) {
+					valorOficial = 0;
+					valueConversorOficial.setText("$ " + format.format(valorOficial));
+				} else {					
+					valorOficial = Float.parseFloat(stringValue) * newOficialSell;
+					valueConversorOficial.setText("$ " + format.format(valorOficial));
+				}
+	
+			}
+		});
+		
+		button_dap.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				
 				labelConversorOficial.setText("Pesos");
-				editValor = (EditText) getView().findViewById(R.id.editValor);
-				tab1.setImageResource(R.drawable.button_selected);
-				tab2.setImageResource(R.drawable.button_unselected);
+				editValor.setText("");
+				valueConversorOficial.setText("0.00");
+				button_dap.setImageResource(R.drawable.button_dap_selected);
+				button_pad.setImageResource(R.drawable.button_pad_unselected);
+				labelConversorBlue.setVisibility(View.INVISIBLE);
+				valueConversorBlue.setVisibility(View.INVISIBLE);
+				lineConversorBlue.setVisibility(View.INVISIBLE);
+				labelConversorCard.setVisibility(View.INVISIBLE);
+				valueConversorCard.setVisibility(View.INVISIBLE);
+				lineConversorCard.setVisibility(View.INVISIBLE);
 
 				calcularButton.setOnClickListener(new OnClickListener() {
 					
@@ -83,22 +122,30 @@ public class CalcTab extends Fragment{
 							valorOficial = Float.parseFloat(stringValue) * newOficialSell;
 							valueConversorOficial.setText("$ " + format.format(valorOficial));
 						}
-										
-										
+			
 					}
 				});				
 			}
 		});
 		
-		labelPesosDolares.setOnClickListener(new OnClickListener() {
+		button_pad.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				
-				labelConversorOficial.setText("Dolares");
-				editValor = (EditText) getView().findViewById(R.id.editValor);
-				tab1.setImageResource(R.drawable.button_unselected);
-				tab2.setImageResource(R.drawable.button_selected);
+				labelConversorOficial.setText("Oficial");
+				editValor.setText("");
+				valueConversorOficial.setText("0.00");
+				valueConversorBlue.setText("0.00");
+				valueConversorCard.setText("0.00");
+				button_dap.setImageResource(R.drawable.button_dap_unselected);
+				button_pad.setImageResource(R.drawable.button_pad_selected);
+				labelConversorBlue.setVisibility(View.VISIBLE);
+				valueConversorBlue.setVisibility(View.VISIBLE);
+				lineConversorBlue.setVisibility(View.VISIBLE);
+				labelConversorCard.setVisibility(View.VISIBLE);
+				valueConversorCard.setVisibility(View.VISIBLE);
+				lineConversorCard.setVisibility(View.VISIBLE);
 
 				calcularButton.setOnClickListener(new OnClickListener() {
 					
@@ -107,14 +154,17 @@ public class CalcTab extends Fragment{
 						imm.hideSoftInputFromWindow(editValor.getWindowToken(), 0);
 						String stringValue = editValor.getText().toString();
 						if (stringValue.equals("")) {
-							valorOficial = 0;
-							valueConversorOficial.setText("$ " + format.format(valorOficial));
+							valueConversorOficial.setText("$ " + format.format(0));
+							valueConversorBlue.setText("$ " + format.format(0));
+							valueConversorCard.setText("$ " + format.format(0));
 						} else {					
 							valorOficial = Float.parseFloat(stringValue) / newOficialSell;
+							valorBlue = Float.parseFloat(stringValue) / newBlueSell;
 							valueConversorOficial.setText("$ " + format.format(valorOficial));
+							valueConversorBlue.setText("$ " + format.format(valorBlue));
+							valueConversorCard.setText("$ " + format.format(valorOficial / 1.35));
 						}
-										
-										
+		
 					}
 				});				
 			}
